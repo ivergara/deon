f41cd90d3c444886af1affb0633c4e29c1300539
Ignacio Vergara Kausel <ivergarakausel@gmail.com>
2018-10-27 21:51:54 +0200

    template modularization, handling of error

--- webapp/app.py
+++ webapp/app.py
@@ -1,12 +1,33 @@
 from pathlib import Path
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 
 import deon
 
 app = Flask(__name__)
 
 DEFAULT_CHECKLIST = Path(__file__).parent.parent / 'deon' / 'assets' / 'checklist.yml'
+
+
+class InvalidUsage(Exception):
+    status_code = 400
+
+    def __init__(self, message, status_code=None, payload=None):
+        Exception.__init__(self)
+        self.message = message
+        if status_code is not None:
+            self.status_code = status_code
+
+
+@app.errorhandler(InvalidUsage)
+def handle_invalid_usage(error):
+    return render_template('400.html', msg=error.message), error.status_code
+
+
+@app.route('/foo')
+def get_foo():
+    raise InvalidUsage('This view is gone', status_code=410)
+
 
 @app.route("/create")
 def create():
@@ -15,6 +36,9 @@ def create():
     else:
         format_ = 'html'
 
-    result = deon.create(DEFAULT_CHECKLIST, format_, None, False, False)
+    try:
+        result = deon.create(DEFAULT_CHECKLIST, format_, None, False, False)
+    except deon.FormatException:
+        raise InvalidUsage(f'Format {format_} not supported or invalid', status_code=400)
 
     return render_template('index.html', format=format_, content=result)
